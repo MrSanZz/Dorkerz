24b1fff6dc5b2e8b07d6664913566b2882d29d88
MrSanZz <95021841+MrSanZz@users.noreply.github.com>
2024-07-09 23:54:45 +0700

    Update dork.py

--- dork.py
+++ dork.py
@@ -5,9 +5,14 @@ try:
     import fake_useragent
     from fake_useragent import UserAgent
     from requests import Timeout
-except ModuleNotFoundError:
+except ModuleNotFoundError as e:
     import os
-    os.system("pip3 install fake_useragent && pip3 install google")
+    if 'fake_useragent' in str(e):
+        os.system('pip3 install fake_useragent')
+    elif 'googlesearch' in str(e):
+        os.system('pip3 install google')
+    else:
+        os.system('pip3 install requests')
 logo="""
 ,------.                ,--.                         
 |  .-.  \  ,---. ,--.--.|  |,-. ,---. ,--.--.,-----. 
@@ -15,13 +20,12 @@ logo="""
 |  '--'  /' '-' '|  |   |  \  \\\   --.|  |    /  `-. 
 `-------'  `---' `--'   `--'`--'`----'`--'   `-----' 
 """
-ua = UserAgent()
 def enter(self):
-    for results in search(dork, num=int(2), user_agent=ua.chrome, stop=None, pause=2, start=0):
+    for results in search(dork, num=int(2), user_agent=UserAgent().chrome, stop=None, pause=2, start=0):
         try:
             headers = {"User-Agent": ua.chrome}
             response = requests.get(results, headers=headers, timeout=9)
-            print(str(response) + '-' + results)
+            print(str(response.status_code) + '-' + results)
         except Timeout:
             print('Timeout' + '-' + results)
         except:
