0f8aff9d09ff670ed0b74c2413c7259d14d4b05e
MrSanZz <95021841+MrSanZz@users.noreply.github.com>
2025-05-08 20:37:50 +0700

    Update dork.py

--- dork.py
+++ dork.py
@@ -21,7 +21,8 @@ logo="""
 `-------'  `---' `--'   `--'`--'`----'`--'   `-----' 
 """
 def enter(self):
-    for results in search(dork, num=int(2), user_agent=UserAgent().chrome, stop=None, pause=2, start=0):
+    global dork
+    for results in search(dork, num=int(1), user_agent=UserAgent().chrome, stop=None, pause=1, start=0):
         try:
             headers = {"User-Agent": ua.chrome}
             response = requests.get(results, headers=headers, timeout=9)
@@ -33,7 +34,6 @@ def enter(self):
     print("Done..")
 if __name__ == '__main__':
     print(logo)
-    global dork
     dork = input("[+] Insert Dork : ")
     print('\n')
     enter(dork)
